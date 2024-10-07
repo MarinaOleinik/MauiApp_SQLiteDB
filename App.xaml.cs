ae9827ad31738bfd0e558bbea284aef7113a4fef
marinaoleinik <marina.oleinik@tthk.ee>
2024-10-07 11:02:23 +0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiApp_SQLiteDB
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿using MauiApp_SQLiteDB;
+
+namespace MauiApp_SQLiteDB
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(TodoItemPage), typeof(TodoItemPage));
+        }
+    }
+}
